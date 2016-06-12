6fae6e0642f4a502da41629796b225d9ee51317b
dviramontes <dviramontes@gmail.com>
2016-06-12 08:57:25 -0600

    init, adds webpack + react + babel + less deps

---
+++ README.md
@@ -0,0 +1,8 @@
+
+### deving
+
+`$ npm install`
+
+`$ npm start`
+
+then visit [http://localhost:3333/](http://localhost:3333/)
---
+++ main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+require('./main.less');
+
+ReactDOM.render(<App />, document.getElementById('mount'))
